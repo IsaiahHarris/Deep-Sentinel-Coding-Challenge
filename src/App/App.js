9029dee14cc882b5ee7a100d1f6e9a3cc8273458
Isaiah Harris <harrisfambam@gmail.com>
2019-03-15 23:43:02 -1000

    create header boiler plate

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
 import './App.scss';
-
+import Header from '../Header';
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
   }
 }
 
